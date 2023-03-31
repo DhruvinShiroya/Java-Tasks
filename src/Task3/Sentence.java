edc81205d8982954d0eaf572eabe09de735bb886
Dhruvin Shiroya <shiroyadhruvinm@gmail.com>
2023-03-31 17:38:14 -0400

    Task 3 complete

---
+++ src/Task3/Sentence.java
@@ -0,0 +1,86 @@
+package Task3;
+
+import java.util.ArrayList;
+
+public class Sentence {
+
+    private StringBuilder sentence;
+
+    private ArrayList<String> oddCharacterWords = new ArrayList<>();
+    private ArrayList<String> evenCharacterWords = new ArrayList<>();
+
+    public Sentence(String sentence) {
+        if(sentence.trim().length() < 3){
+            throw new IllegalArgumentException("Please Enter valid length word");
+        }else {
+            this.sentence = new StringBuilder(sentence.trim());
+            if(this.sentence.length() % 2 == 0){
+                evenCharacterWords.add(this.sentence.toString());
+            }else{
+                oddCharacterWords.add(this.sentence.toString());
+            }
+        }
+    }
+
+    public Sentence(StringBuilder sentence) {
+        if(sentence.toString().trim().length() < 3) {
+            throw new IllegalArgumentException("Please Enter valid length word");
+        }else{
+
+            this.sentence = sentence;
+
+            if(this.sentence.length() % 2 == 0){
+                evenCharacterWords.add(this.sentence.toString());
+            }else {
+                oddCharacterWords.add(this.sentence.toString());
+            }
+        }
+    }
+
+    public Sentence() {
+        this.sentence = new StringBuilder("");
+    }
+
+    public String getLastWord() {
+        return sentence.toString();
+    }
+
+    public ArrayList<String> getOddCharacterWords() {
+        return oddCharacterWords;
+    }
+
+    public ArrayList<String> getEvenCharacterWords() {
+        return evenCharacterWords;
+    }
+
+    public void setSentence(StringBuilder sentence) {
+        if(sentence.toString().trim().length() < 3) {
+            throw new IllegalArgumentException("Please Enter valid length word");
+        }else{
+            String temp = sentence.toString().trim();
+            this.sentence.setLength(0);
+            this.sentence = this.sentence.append(temp);
+
+            if(this.sentence.length() % 2 == 0){
+                evenCharacterWords.add(this.sentence.toString());
+            }else {
+                oddCharacterWords.add(this.sentence.toString());
+            }
+        }
+    }
+
+    public void setSentence(String sentence) {
+        if(sentence.trim().length() < 3) {
+            throw new IllegalArgumentException("Please Enter valid length word");
+        }else{
+            this.sentence.setLength(0);
+            this.sentence = this.sentence.append(sentence.trim());
+
+            if(this.sentence.length() % 2 == 0){
+                evenCharacterWords.add(this.sentence.toString());
+            }else {
+                oddCharacterWords.add(this.sentence.toString());
+            }
+        }
+    }
+}
---
+++ src/Task3/SentenceRunner.java
@@ -0,0 +1,41 @@
+package Task3;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SentenceRunner {
+
+    public static void main(String[] args) {
+
+        Sentence sentence = new Sentence();
+
+        boolean moreInput = true;
+
+        while(moreInput){
+
+            Scanner userInput = new Scanner(System.in);
+
+            // ask user for input word
+            System.out.println("Plese Enter word you want? ");
+            sentence.setSentence(userInput.nextLine());
+
+
+            // ask user if he want to quit this
+
+            System.out.println("Enter no if you want to quit this");
+
+            //if use answer no for more input
+            if(userInput.nextLine().toLowerCase().equals("no")){
+                moreInput = false;
+            }
+        }
+
+        System.out.println("this is odd length word list word you have entered");
+        System.out.println(sentence.getOddCharacterWords().toString());
+        System.out.println("_______________________________________________________");
+        System.out.println("this is even length word list word you have entered");
+        System.out.println(sentence.getEvenCharacterWords().toString());
+        System.out.println("_______________________________________________________");
+    }
+
+}
